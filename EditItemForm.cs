2f13c53375eb2acc4ca9c393f742c147fbe1ccad
TOM HAMMOND <T.J.HAMMOND-2021@hull.ac.uk>
2025-08-23 00:03:11 +0100

    Created a seperate form to make it simpler and much easier to edit items

---
+++ EditItemForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_Managment_System_Forms
+{
+    public partial class EditItemForm : Form
+    {
+        public InventoryItem UpdatedItem { get; private set; }
+
+        public EditItemForm(InventoryItem item)
+        {
+            InitializeComponent();
+            // Pre-fill fields
+            txtName.Text = item.Name;
+            txtCategory.Text = item.Category;
+            nudQuantity.Value = item.Quantity;
+            txtPrice.Text = item.Price.ToString(CultureInfo.InvariantCulture);
+
+            // Store the original ID
+            UpdatedItem = new InventoryItem(item.Id, item.Name, item.Category, item.Quantity, item.Price);
+        }
+
+        private void EditItemForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string category = txtCategory.Text;
+            int quantity = (int)nudQuantity.Value;
+
+            if (!decimal.TryParse(txtPrice.Text, out decimal price))
+            {
+                MessageBox.Show("Invalid price!");
+                return;
+            }
+
+            // Update item
+            UpdatedItem.Name = name;
+            UpdatedItem.Category = category;
+            UpdatedItem.Quantity = quantity;
+            UpdatedItem.Price = price;
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
