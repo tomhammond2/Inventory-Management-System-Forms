d9360baaabfd5dfe65d07af8c527c3d95d8364db
TOM HAMMOND <T.J.HAMMOND-2021@hull.ac.uk>
2025-08-23 00:03:57 +0100

    Added code which allows for adding, deleting, editing and veiwing of iventroy

---
+++ InventroyViewer.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Inventory_Managment_System_Forms
+{
+    partial class InventroyViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGrid = new System.Windows.Forms.DataGridView();
+            this.AddItemBtn = new System.Windows.Forms.Button();
+            this.SaveDataBtn = new System.Windows.Forms.Button();
+            this.EditItemBtn = new System.Windows.Forms.Button();
+            this.DeleteItemBtn = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SearchDataBtn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.cmbSearchType = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGrid
+            // 
+            this.DataGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.DataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGrid.Location = new System.Drawing.Point(16, 26);
+            this.DataGrid.Name = "DataGrid";
+            this.DataGrid.Size = new System.Drawing.Size(775, 300);
+            this.DataGrid.TabIndex = 0;
+            // 
+            // AddItemBtn
+            // 
+            this.AddItemBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddItemBtn.Location = new System.Drawing.Point(18, 355);
+            this.AddItemBtn.Name = "AddItemBtn";
+            this.AddItemBtn.Size = new System.Drawing.Size(179, 63);
+            this.AddItemBtn.TabIndex = 1;
+            this.AddItemBtn.Text = "Add Item";
+            this.AddItemBtn.UseVisualStyleBackColor = true;
+            this.AddItemBtn.Click += new System.EventHandler(this.AddItemBtn_Click);
+            // 
+            // SaveDataBtn
+            // 
+            this.SaveDataBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveDataBtn.Location = new System.Drawing.Point(612, 355);
+            this.SaveDataBtn.Name = "SaveDataBtn";
+            this.SaveDataBtn.Size = new System.Drawing.Size(179, 63);
+            this.SaveDataBtn.TabIndex = 2;
+            this.SaveDataBtn.Text = "Save";
+            this.SaveDataBtn.UseVisualStyleBackColor = true;
+            this.SaveDataBtn.Click += new System.EventHandler(this.SaveDataBtn_Click_1);
+            // 
+            // EditItemBtn
+            // 
+            this.EditItemBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditItemBtn.Location = new System.Drawing.Point(213, 355);
+            this.EditItemBtn.Name = "EditItemBtn";
+            this.EditItemBtn.Size = new System.Drawing.Size(179, 63);
+            this.EditItemBtn.TabIndex = 3;
+            this.EditItemBtn.Text = "Edit Item";
+            this.EditItemBtn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteItemBtn
+            // 
+            this.DeleteItemBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteItemBtn.Location = new System.Drawing.Point(410, 355);
+            this.DeleteItemBtn.Name = "DeleteItemBtn";
+            this.DeleteItemBtn.Size = new System.Drawing.Size(179, 63);
+            this.DeleteItemBtn.TabIndex = 4;
+            this.DeleteItemBtn.Text = "Delete Item";
+            this.DeleteItemBtn.UseVisualStyleBackColor = true;
+            this.DeleteItemBtn.Click += new System.EventHandler(this.DeleteItemBtn_Click_1);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(18, 521);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(179, 20);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 494);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Phrase";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(209, 495);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Name or Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 453);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Search/Filter";
+            // 
+            // SearchDataBtn
+            // 
+            this.SearchDataBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchDataBtn.Location = new System.Drawing.Point(410, 511);
+            this.SearchDataBtn.Name = "SearchDataBtn";
+            this.SearchDataBtn.Size = new System.Drawing.Size(105, 32);
+            this.SearchDataBtn.TabIndex = 10;
+            this.SearchDataBtn.Text = "Search";
+            this.SearchDataBtn.UseVisualStyleBackColor = true;
+            this.SearchDataBtn.Click += new System.EventHandler(this.SearchDataBtn_Click);
+            // 
+            // cmbSearchType
+            // 
+            this.cmbSearchType.FormattingEnabled = true;
+            this.cmbSearchType.Items.AddRange(new object[] {
+            "Name",
+            "Category"});
+            this.cmbSearchType.Location = new System.Drawing.Point(213, 521);
+            this.cmbSearchType.Name = "cmbSearchType";
+            this.cmbSearchType.Size = new System.Drawing.Size(158, 21);
+            this.cmbSearchType.TabIndex = 11;
+            // 
+            // InventroyViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 554);
+            this.Controls.Add(this.cmbSearchType);
+            this.Controls.Add(this.SearchDataBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.DeleteItemBtn);
+            this.Controls.Add(this.EditItemBtn);
+            this.Controls.Add(this.SaveDataBtn);
+            this.Controls.Add(this.AddItemBtn);
+            this.Controls.Add(this.DataGrid);
+            this.Name = "InventroyViewer";
+            this.Text = "Inventroy Manager";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataGrid;
+        private System.Windows.Forms.Button AddItemBtn;
+        private System.Windows.Forms.Button SaveDataBtn;
+        private System.Windows.Forms.Button EditItemBtn;
+        private System.Windows.Forms.Button DeleteItemBtn;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SearchDataBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox cmbSearchType;
+    }
+}
+
---
+++ InventroyViewer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Inventory_Managment_System_Forms
+{
+    public partial class InventroyViewer : Form
+    {
+
+        private Inventory inventory;
+
+        public InventroyViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            inventory = new Inventory();
+
+            // Load items from CSV
+            foreach (var item in FileHandler.Load())
+                inventory.AddItem(item.Name, item.Category, item.Quantity, item.Price);
+
+            DataGrid.AutoGenerateColumns = true;
+            RefreshGrid();
+            EditItemBtn.Click += EditItemBtn_Click;
+        }
+
+        private void RefreshGrid()
+        { 
+            DataGrid.DataSource = null;
+            DataGrid.DataSource = inventory.GetAllItems();
+
+            // Adjust column sizes
+            DataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DataGrid.ColumnHeadersDefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            DataGrid.AutoResizeColumnHeadersHeight();
+            DataGrid.RowTemplate.Height = 30; // optional, makes rows taller
+        }
+
+ 
+
+        private void AddItemBtn_Click(object sender, EventArgs e)
+        {
+            using (var form = new AddItemForm())
+            {
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    inventory.AddItem(form.NewItem.Name, form.NewItem.Category, form.NewItem.Quantity, form.NewItem.Price);
+                    RefreshGrid();
+                }
+            }
+        }
+
+        private void EditItemBtn_Click(object sender, EventArgs e)
+        {
+            if (DataGrid.CurrentRow == null) return;
+
+            var selectedItem = (InventoryItem)DataGrid.CurrentRow.DataBoundItem;
+
+            using (var form = new EditItemForm(selectedItem))
+            {
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    // Update inventory with new values
+                    inventory.EditItem(
+                        form.UpdatedItem.Id,
+                        form.UpdatedItem.Name,
+                        form.UpdatedItem.Category,
+                        form.UpdatedItem.Quantity,
+                        form.UpdatedItem.Price
+                    );
+
+                    RefreshGrid();
+                }
+            }
+        }
+
+ 
+
+        private void DeleteItemBtn_Click_1(object sender, EventArgs e)
+        {
+            if (DataGrid.CurrentRow == null)
+            {
+                MessageBox.Show("Please select an item to delete.");
+                return;
+            }
+
+            var selectedItem = (InventoryItem)DataGrid.CurrentRow.DataBoundItem;
+
+            var confirm = MessageBox.Show($"Are you sure you want to delete '{selectedItem.Name}'?",
+                                          "Confirm Delete", MessageBoxButtons.YesNo);
+
+            if (confirm == DialogResult.Yes)
+            {
+                inventory.DeleteItem(selectedItem.Id);
+                RefreshGrid();
+                FileHandler.Save(inventory.GetAllItems());
+            }
+        }
+
+        private void SaveDataBtn_Click_1(object sender, EventArgs e)
+        {
+            FileHandler.Save(inventory.GetAllItems());
+            MessageBox.Show("Inventory saved!");
+        }
+
+        private void SearchDataBtn_Click(object sender, EventArgs e)
+        {
+            string query = txtSearch.Text;
+            List<InventoryItem> results;
+
+            if (cmbSearchType.SelectedItem.ToString() == "Name")
+                results = inventory.SearchByName(query);
+            else // Category
+                results = inventory.SearchByCategory(query);
+
+            DataGrid.DataSource = null;
+            DataGrid.DataSource = results;
+        }
+    }
+}
