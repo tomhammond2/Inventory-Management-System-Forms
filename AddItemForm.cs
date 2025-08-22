9b3eb56026e7515dcfa127014a6983c60a64deb9
TOM HAMMOND <T.J.HAMMOND-2021@hull.ac.uk>
2025-08-23 00:01:20 +0100

    Created a seperate form to make it simpler and much easier to edit items

---
+++ AddItemForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_Managment_System_Forms
+{
+    public partial class AddItemForm : Form
+    {
+        public InventoryItem NewItem { get; private set; }
+
+        public AddItemForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string category = txtCategory.Text;
+            int quantity = (int)nudQuantity.Value;
+
+            if (!decimal.TryParse(txtPrice.Text, out decimal price))
+            {
+                MessageBox.Show("Invalid price!");
+                return;
+            }
+
+            NewItem = new InventoryItem(0, name, category, quantity, price); // Id will be set by Inventory class
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
